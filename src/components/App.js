166058015bd72ec277e0e85f9bd2a2802118ac9a
sriramulagopi <129825285+sriramulagopi@users.noreply.github.com>
2024-07-14 14:11:44 +0000

    solution

--- src/components/App.js
+++ src/components/App.js
@@ -1,11 +1,22 @@
 
-import React from "react";
+import React, { useState } from "react";
 import './../styles/App.css';
 
 const App = () => {
+  const [text,setText]=useState();
+  const [text1,setText1]=useState();
+  const [text2,setText2]=useState();
+  const onChange = (e)=>{
+    setText1("Hello ");
+    setText2("!");
+    setText(e.target.value);
+  }
   return (
     <div>
         {/* Do not remove the main div */}
+        <label>Enter your name:</label>
+        <input type="text"  onChange={onChange}/>
+        <p>{text1}{text}{text2}</p>
     </div>
   )
 }
